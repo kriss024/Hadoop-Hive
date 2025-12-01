54a5f29926aaba5799d76ce588cb66ae1163009b
Krzysztof Bruszewski <krzysztof.bruszewski@gmail.com>
2025-12-01 20:17:25 +0100

    Change tables to EXTERNAL and update views

--- Hive.sql
+++ Hive.sql
@@ -21,18 +21,23 @@ PARTITIONED BY (column_name)
 STORED AS PARQUET
 AS SELECT * FROM <table_name2>;
 
-CREATE TABLE IF NOT EXISTS <database_name>.<table_name>(txn_id BIGINT, cust_id INT, amount DECIMAL(20,2),txn_type STRING, created_date DATE)
+CREATE EXTERNAL TABLE IF NOT EXISTS <database_name>.<table_name>(txn_id BIGINT, cust_id INT, amount DECIMAL(20,2),txn_type STRING, created_date DATE)
 COMMENT 'A table to store transactions'
 PARTITIONED BY (txn_date DATE)
 STORED AS PARQUET;
 
-CREATE TABLE IF NOT EXISTS <database_name>.<table_name>(txn_id BIGINT, cust_id INT, amount DECIMAL(20,2), created_date DATE)
+CREATE EXTERNAL TABLE IF NOT EXISTS <database_name>.<table_name>(txn_id BIGINT, cust_id INT, amount DECIMAL(20,2), created_date DATE)
 COMMENT 'A table to store transactions'
 PARTITIONED BY (txn_date DATE, txn_type STRING)
 STORED AS PARQUET;
 
 DROP TABLE IF EXISTS <view_name>;
-CREATE VIEW IF NOT EXISTS <view_name> AS SELECT1;
+
+CREATE OR REPLACE VIEW <view_name_new> 
+AS SELECT * FROM <view_name>;
+
+CREATE VIEW IF NOT EXISTS <view_name_new> 
+AS SELECT * FROM <view_name>;
 
 SELECT CAST(report_date AS STRING FORMAT 'YYYY-MM') AS report_date
 
